f1d56637f3f86aab97cfe03df5043f2a123c0044
j_polak <jan.polak@onmyway.cz>
2017-02-10 12:59:23 +0100

    Day 25 wrap up

--- system/core/libraries/view.class.php
+++ system/core/libraries/view.class.php
@@ -6,6 +6,8 @@ class view
     // contact/form ... /system/project/views/contact/form.php
     protected $template_name = null;
 
+    public $template_variables = array();
+
     /**
      * 
      */
@@ -19,14 +21,39 @@ class view
      */
     public function render($print = true)
     {
+        $rendered_content = $this->getRenderedContent();
+
         if($print)
         {
             // echo the contents of the file
-            include VIEWS_DIR . '/' . $this->template_name . '.php';
+            echo $rendered_content;
         }
         else
         {
             // return the contents of the file
+            return $rendered_content;
         }
+    }
+
+    protected function getRenderedContent()
+    {
+        // start output buffering (start 'recording' any output)
+        ob_start();
+
+        // turn the array into variables (keys become names of variables)
+        extract($this->template_variables);
+
+        /*
+        $name; // 'jan.polak@data4you.cz'
+        $text; // 'Hello!'
+        $newsletter; // 1
+        */
+
+        // include the actual file (WITHOUT output buffering this would be immediately printed out)
+        // but WITH the output buffering it is just stored in the buffer (in the 'recording')
+        include VIEWS_DIR . '/' . $this->template_name . '.php';
+
+        // get the contents of the output buffer (the 'recording') and clean the buffer
+        return ob_get_clean();
     }
 }
--- system/project/controllers/contact.controller.php
+++ system/project/controllers/contact.controller.php
@@ -1,12 +1,23 @@
 <?php
 
+$email = request::post('email', '');
+$text = request::post('text', '');
+$newsletter = request::post('newsletter', '0');
+
 //  /system/project/views/contact/form.php
 //  contact/form
 $form_view = new view('contact/form');
+$form_view->template_variables['email'] = $email;
+$form_view->template_variables['text'] = $text;
+$form_view->template_variables['newsletter'] = $newsletter;
+// $form_view->newsletter = $newsletter;
+
+
 var_dump($form_view);
 
 // example of retrieving the rendered HTML and not printing it 
 $html = $form_view->render(false);
+//var_dump($html);
 
 ?>
 <!DOCTYPE html>
